f2cf88b57b7c3a95535b5e081df750a6fc8713fe
cesarspichencoff-cmyk <cesar.spichencoff@gmail.com>
2026-06-16 02:38:19 -0300

    "fix: adiciona componentes faltantes"

---
+++ src/components/Toast.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export function toast(_msg: string) {
+  console.log('toast:', _msg);
+}
+
+export function ToastHost() {
+  return null;
+}
---
+++ src/components/cardapio/PosterSemana.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export function PosterSemana() {
+  return null;
+}
